f16ea64a161e14f100c2f73f580be4e07277c8f2
Thomas Cardwell <79612290+tdgcardwell@users.noreply.github.com>
2022-01-13 14:45:32 -0500

    dropped in js template for storage etc. added JQuery

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// ========== On page load:
+
+function checkAndDisplay(){
+
+  // check localstorage for saved information
+
+  // IF there is stored take info, then add it to the page.
+
+};
+
+checkAndDisplay();
+
+
+// ========== When Log button clicked (or form submitted):
+
+on submit {
+
+  // prevent default
+
+  // make an array of the information
+
+  // add it to local storage
+
+  checkAndDisplay();
+
+};
+
+
+
+
+
+
 //warn and confirm before close or refresh
 window.addEventListener('beforeunload', function (e) {
     e.preventDefault();
@@ -23,16 +55,180 @@ const month = date.getMonth();
 const day = date.getDate();
 
 // focus scene input on page load
-scene.focus();
+// scene.focus();
 
-//add date to header
-let heading = document.querySelector('h1');
-heading.textContent = `${heading.textContent} - ${month}/${day}/${year}`;
 
 //reset function
 function reset(field) {
   field.value = "";
 }
+
+
+/*
+======================================================
+updating
+======================================================
+*/
+
+// an array with info of new members - this is how take info should be stored
+let newMembers = [
+  {name: "Victoria Chambers",
+  number: 1,
+  email: "victoria.chambers80@example.com",
+  joinDate: "12/15/21"},
+  {name: "Algernon Fudginton",
+  number: 2,
+  email: "yourbrotherken@example.com",
+  joinDate: "11/15/21"},
+  {name: "Juliet Montague",
+  number: 3,
+  email: "jules.capulet92@example.com",
+  joinDate: "10/15/21"},
+  {name: "Sven Hasselbläd",
+  number: 4,
+  email: "77hesitancy@example.com",
+  joinDate: "09/15/21"}
+];
+
+// something to collect the info...
+let membersToInsert = "";
+
+// loop through each, building info
+for (let i=0; i<newMembers.length; i++) {
+  let eachOne = `<div class="new-member"><img class="avatar" src="imgs/members/member-${newMembers[i]["number"]}.jpg" alt='${newMembers[i]["name"]}'><div><h4>${newMembers[i]["name"]}</h4><span class="email">${newMembers[i]["email"]}</span></div><span class="date">${newMembers[i]["joinDate"]}</span></div>`;
+  membersToInsert += eachOne;
+}
+
+// add it.
+$('#new-members h3').after(membersToInsert);
+
+/*
+
+/*
+======================================================
+/updating
+======================================================
+*/
+
+/*
+======================================================
+storage
+======================================================
+*/
+
+// gotta declare it outside of a function
+let storedSettings;
+
+// if there are stored settings...
+if (localStorage.length > 0) {
+  // ... get those stored settings
+  storedSettings = JSON.parse(localStorage.getItem('locallyStored'));
+  console.log('settings retrieved');
+} else {
+  // otherwise default to false for both, and 0 for timezone
+  storedSettings = {
+    emailNotifications : false,
+    setProfileToPublic : false,
+    timeZone: 0
+  };
+}
+
+// and adjust the sliders
+let emailNotifications = $('#emailNotifications');
+let setProfileToPublic = $('#setProfileToPublic');
+
+if (storedSettings.emailNotifications) {
+  emailNotifications.prop("checked", true);
+}
+
+if (storedSettings.setProfileToPublic) {
+  setProfileToPublic.prop("checked", true);
+}
+
+// set the menu
+// we want to filter by timeZoneId
+let storedZone = $('#time_zones option').filter('[timeZoneId =' + storedSettings.timeZone + ']');
+// then we want to give that one the property 'selected'
+storedZone.prop("selected", true);
+
+
+// googled "jquery boolean checkbox" to see how to concisely do that. which got me to here: https://stackoverflow.com/questions/37301563/how-to-get-bool-value-from-checkbox-in-javascript-jquery - Which I then adapted for my needs.
+
+emailNotifications.change(function() {
+  if ($(this).is(":checked")) {
+    storedNotify = true;
+  } else {
+    storedNotify = false;
+  }
+  storedSettings.emailNotifications = storedNotify;
+  // console.log(storedSettings);
+});
+
+setProfileToPublic.change(function() {
+  if ($(this).is(":checked")) {
+    storedPublic = true;
+  } else {
+    storedPublic = false;
+  }
+  storedSettings.setProfileToPublic = storedPublic;
+  // console.log(storedSettings);
+});
+
+// what about the timezone?
+let timezones = $('#time_zones');
+let timezone;
+
+// this is super helpful: https://oscarotero.com/jquery/ !!
+
+timezones.change(function(){
+  // attr this time!
+  timezone = $('#time_zones option').filter(':selected').attr("timeZoneId");
+  // alert(timezone);
+  storedSettings.timeZone = timezone;
+})
+
+
+// save the stored settings to local storage
+
+// need to remember to ADD not overwrite these.
+
+function saveSettings() {
+  localStorage.setItem('locallyStored', JSON.stringify(storedSettings));
+  console.log('settings saved');
+  alert('settings saved');
+};
+
+
+// let's mess with these buttons
+let saveButton = $('#saveButton')
+saveButton.click(function() {
+  event.preventDefault();
+  saveSettings();
+});
+
+let cancelButton = $('#cancelButton')
+cancelButton.click(function() {
+  event.preventDefault();
+  localStorage.clear()
+
+  // reset the sliders
+  emailNotifications.prop("checked", false);
+  setProfileToPublic.prop("checked", false);
+
+  // reset the menu
+  // we want to filter by timeZoneId
+  let zoneReset = $('#time_zones option').filter('[timeZoneId ="0"]');
+  // then we want to give that one the property 'selected'
+  zoneReset.prop("selected", true);
+  console.log('settings reset');
+  alert('settings reset');
+});
+
+/*
+======================================================
+/storage
+======================================================
+*/
 
 
 //log a scene
@@ -87,6 +283,14 @@ logIt.addEventListener('submit', (e)=> {
 
 //reset fields when changing shot
 shot.addEventListener('input' , (e) => {
+  take.value = 1;
+  // reset(lens);
+  // iso.value = 800;
+  // fStop.value = 'ƒ1.8';
+})
+
+//reset take when changing scene
+scene.addEventListener('input' , (e) => {
   take.value = 1;
   // reset(lens);
   // iso.value = 800;
