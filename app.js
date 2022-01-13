80f9dbb18ab08d44da2b41743d040789fe0ca59d
Thomas Cardwell <79612290+tdgcardwell@users.noreply.github.com>
2022-01-13 16:29:05 -0500

    more messing w/ JS

--- app.js
+++ app.js
@@ -30,7 +30,25 @@ function checkAndDisplay(){
 
     // loop through each, building info
     for (let i=0; i<storedTakes.length; i++) {
-      let eachOne = `<div class="new-member"><img class="avatar" src="imgs/members/member-${newMembers[i]["number"]}.jpg" alt='${newMembers[i]["name"]}'><div><h4>${newMembers[i]["name"]}</h4><span class="email">${newMembers[i]["email"]}</span></div><span class="date">${newMembers[i]["joinDate"]}</span></div>`;
+
+      let eachOne;
+
+      let year = date.getFullYear();
+      let month = date.getMonth();
+      let day = date.getDate();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+
+      let takeInfo =
+                      ` <div class="">${scene.value}</div>
+                        <div class="">${shot.value.toUpperCase()}</div>
+                        <div class="">${take.value}</div>
+                        <div class="">${lens.value}</div>
+                        <div class="">${iso.value}</div>
+                        <div class="">ƒ${fStop.value}</div>
+                        <div class="">${notes.value}</div>
+                        <div class="">${hours}:${minutes}</div>`;
+
       takesToInsert += eachOne;
     }
 
@@ -88,6 +106,33 @@ logIt.addEventListener('submit', (e)=> {
   }
 
   // add it to local storage
+  // need to remember to ADD not overwrite these.
+  function saveSettings() {
+
+    // get existing
+    storedTakes = JSON.parse(localStorage.getItem('locallyStored'));
+
+    // add thisTake to the end of storedTakes
+    storedTakes.push(thisTake);
+
+    // save it back again
+    localStorage.setItem('locallyStored', JSON.stringify(storedTakes));
+    console.log('take saved');
+  };
+
+  // increment take number
+  take.value ++;
+
+  // reset other relevant fields
+  reset(scene);
+  reset(shot);
+  reset(notes);
+
+  // reset the checkbox
+  circle.checked = false;
+
+  //focus shot input
+  shot.focus();
 
   // then update the displayed info
   checkAndDisplay();
@@ -95,126 +140,7 @@ logIt.addEventListener('submit', (e)=> {
 });
 
 
-/*
-======================================================
-updating
-======================================================
-*/
-
-// an array with info of new members - this is how take info should be stored
-let newMembers = [
-  {name: "Victoria Chambers",
-  number: 1,
-  email: "victoria.chambers80@example.com",
-  joinDate: "12/15/21"},
-  {name: "Algernon Fudginton",
-  number: 2,
-  email: "yourbrotherken@example.com",
-  joinDate: "11/15/21"},
-  {name: "Juliet Montague",
-  number: 3,
-  email: "jules.capulet92@example.com",
-  joinDate: "10/15/21"},
-  {name: "Sven Hasselbläd",
-  number: 4,
-  email: "77hesitancy@example.com",
-  joinDate: "09/15/21"}
-];
-
-// something to collect the info...
-let membersToInsert = "";
-
-// loop through each, building info
-for (let i=0; i<newMembers.length; i++) {
-  let eachOne = `<div class="new-member"><img class="avatar" src="imgs/members/member-${newMembers[i]["number"]}.jpg" alt='${newMembers[i]["name"]}'><div><h4>${newMembers[i]["name"]}</h4><span class="email">${newMembers[i]["email"]}</span></div><span class="date">${newMembers[i]["joinDate"]}</span></div>`;
-  membersToInsert += eachOne;
-}
-
-// add it.
-$('#new-members h3').after(membersToInsert);
-
-/*
-
-/*
-======================================================
-/updating
-======================================================
-*/
-
-/*
-======================================================
-storage
-======================================================
-*/
-
-
-
-// and adjust the sliders
-let emailNotifications = $('#emailNotifications');
-let setProfileToPublic = $('#setProfileToPublic');
-
-if (storedSettings.emailNotifications) {
-  emailNotifications.prop("checked", true);
-}
-
-if (storedSettings.setProfileToPublic) {
-  setProfileToPublic.prop("checked", true);
-}
-
-// set the menu
-// we want to filter by timeZoneId
-let storedZone = $('#time_zones option').filter('[timeZoneId =' + storedSettings.timeZone + ']');
-// then we want to give that one the property 'selected'
-storedZone.prop("selected", true);
-
-
-// googled "jquery boolean checkbox" to see how to concisely do that. which got me to here: https://stackoverflow.com/questions/37301563/how-to-get-bool-value-from-checkbox-in-javascript-jquery - Which I then adapted for my needs.
-
-emailNotifications.change(function() {
-  if ($(this).is(":checked")) {
-    storedNotify = true;
-  } else {
-    storedNotify = false;
-  }
-  storedSettings.emailNotifications = storedNotify;
-  // console.log(storedSettings);
-});
-
-setProfileToPublic.change(function() {
-  if ($(this).is(":checked")) {
-    storedPublic = true;
-  } else {
-    storedPublic = false;
-  }
-  storedSettings.setProfileToPublic = storedPublic;
-  // console.log(storedSettings);
-});
-
-// what about the timezone?
-let timezones = $('#time_zones');
-let timezone;
-
-// this is super helpful: https://oscarotero.com/jquery/ !!
-
-timezones.change(function(){
-  // attr this time!
-  timezone = $('#time_zones option').filter(':selected').attr("timeZoneId");
-  // alert(timezone);
-  storedSettings.timeZone = timezone;
-})
-
-
-// save the stored settings to local storage
-
-// need to remember to ADD not overwrite these.
-
-function saveSettings() {
-  localStorage.setItem('locallyStored', JSON.stringify(storedSettings));
-  console.log('settings saved');
-  alert('settings saved');
-};
-
-
+// ---------------------------------------------------------------
 // let's mess with these buttons
 let saveButton = $('#saveButton')
 saveButton.click(function() {
@@ -239,12 +165,7 @@ cancelButton.click(function() {
   console.log('settings reset');
   alert('settings reset');
 });
-
-/*
-======================================================
-/storage
-======================================================
-*/
+// ---------------------------------------------------------------
 
 
 
@@ -284,21 +205,7 @@ cancelButton.click(function() {
   );
 
 
-  // increment take number
-  take.value ++;
-
-  // reset other relevant fields
-  reset(scene);
-  reset(shot);
-  reset(notes);
-
-  // reset the checkbox
-  circle.checked = false;
-
-  //focus shot input
-  shot.focus();
-
-
+// =================================================
 
 //reset fields when changing shot
 shot.addEventListener('input' , (e) => {
